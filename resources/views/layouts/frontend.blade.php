83812339571e294d407817031e344a0217c5a461
fadhadwa <fadhadwahyu30@gmail.com>
2023-06-12 00:18:44 +0700

    nambah logo dan ubah colour jadi orange

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="zxx">
+<html lang="eng">
   <head>
     <meta charset="UTF-8" />
-    <meta name="description" content="Ogani Template" />
-    <meta name="keywords" content="Ogani, unica, creative, html" />
+    <meta name="description" content="Senjanis" />
+    <meta name="keywords" content="senjanis" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Ogani | Template</title>
+    <title>Senjanis</title>
 
     <!-- Google Font -->
     <link
